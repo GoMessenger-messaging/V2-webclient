1aeb0d71bfc05f4d9b62550110f075770d07b1b7
Jeroen Leuenberger <jereileu@proton.me>
2024-05-04 22:45:34 +0200

    Add log in and sign up html content

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -7,13 +7,32 @@
   if (username) {
     router.back();
   }
+
+  const buttons = [
+    {
+      "to": "/signup",
+      "icon": "/icons/user-plus.svg",
+      "text": "Sign up instead"
+    }
+  ];
+
+  const inputUsername = "";
+  const inputPassword = "";
 </script>
 
 <template>
   <div>
-    <h1>
-      Log in
-    </h1>
+    <ContentHeader title="Log in" :buttons="buttons" />
+    <div class="flex flex-col items-center">
+      <h3 class="font-bold text-xl">Log in to GoMessenger</h3>
+      <div class="rounded-2xl p-4 mt-4 bg-gray-100">
+        <label for="username" class="font-bold">Email or username</label><br>
+        <input v-model="inputUsername" type="text" id="username" class="rounded border border-gray-800"><br>
+        <label for="password" class="font-bold">Password</label><br>
+        <input v-model="inputPassword" type="password" id="password" class="rounded border border-gray-800"><br>
+        <button type="submit" class="btn mt-4">Log in</button>
+      </div>
+    </div>
   </div>
 </template>
 
--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -7,13 +7,41 @@
   if (username) {
     router.back();
   }
+
+  const buttons = [
+    {
+      "to": "/login",
+      "icon": "/icons/arrow-right-end-on-rectangle.svg",
+      "text": "Log in instead"
+    }
+  ];
+
+  const inputUsername = "";
+  const inputPassword = "";
+  const inputPasswordConfirm = "";
+  const inputEmail = "";
+  const inputDisplayName = "";
 </script>
 
 <template>
   <div>
-    <h1>
-      Sign up
-    </h1>
+    <ContentHeader title="Sign up" :buttons="buttons" />
+    <div class="flex flex-col items-center">
+      <h3 class="font-bold text-xl">Sign up to GoMessenger</h3>
+      <div class="rounded-2xl p-4 mt-4 bg-gray-100">
+        <label for="username" class="font-bold">Username</label><br>
+        <input v-model="inputUsername" type="text" id="username" class="rounded border border-gray-800"><br>
+        <label for="password" class="font-bold">Password</label><br>
+        <input v-model="inputPassword" type="password" id="password" class="rounded border border-gray-800"><br>
+        <label for="passwordConfirm" class="font-bold">Confirm Password</label><br>
+        <input v-model="inputPasswordConfirm" type="password" id="passwordConfirm" class="rounded border border-gray-800"><br>
+        <label for="email" class="font-bold">Email</label><br>
+        <input v-model="inputEmail" type="email" id="email" class="rounded border border-gray-800"><br>
+        <label for="displayName" class="font-bold">Display name</label><br>
+        <input v-model="inputDisplayName" type="text" id="displayName" class="rounded border border-gray-800"><br>
+        <button type="submit" class="btn mt-4">Sign up</button>
+      </div>
+    </div>
   </div>
 </template>
 
